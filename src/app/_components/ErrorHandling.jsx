332569ef220cc06f26dac51d81f6c4afbade1f0e
hesham <heshamabdelazim3@gmail.com>
2025-07-20 16:48:56 +0300

    create-error-animation + responsive-screens-forError

--- src/app/_components/ErrorHandling.jsx
+++ src/app/_components/ErrorHandling.jsx
@@ -13,16 +13,18 @@ function ErrorHandling({ children }) {
       {children}
       {isError && (
         <div
-          className="bg-[white] bg-opacity-5 fixed z-50 inset-0 animate-none backdrop-blur-[1.2px]"
+          className=" popp-bg bg-[white] bg-opacity-5 fixed z-50 inset-0 backdrop-blur-[1.2px]"
           onClick={() => {
             dispatch(setError(null));
           }}
         >
-          <div className=" pop-up bg-opa shadow-[0_0_20px_gray] text-lightOrange bg-secondBackGround">
-            <p className="font-bold">Sorry, Something went wrong</p>
-            <p className="text-4xl">{isError}</p>
+          <div className=" popp w-[90%] md:w-[50%] min-h-[15vh] shadow-[0_0_20px_gray] text-lightOrange bg-secondBackGround">
+            <p className="font-bold text-xl lg:text-4xl ">
+              Sorry, Something went wrong
+            </p>
+            <p className="text-lg md:text-2xl">{isError}</p>
             <button
-              className="absolute left-[101%] top-[-1%] p-2 rounded bg-secondBackGround hover:bg-lightOrange hover:text-white transition"
+              className="absolute left-[80%] md:left-[101%] top-[-35%] md:top-[-1%] p-2 rounded bg-secondBackGround hover:bg-lightOrange hover:text-white transition"
               onClick={() => {
                 dispatch(setError(null));
               }}
